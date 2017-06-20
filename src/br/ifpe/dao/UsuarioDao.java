8f13cd803b753b896ce0fa89d36eefe6a11683f8
sd844rafael <sd844rafael@gmail.com>
2017-06-20 20:26:42 -0300

    2 novos metodos no UsuarioDao

--- src/br/ifpe/dao/UsuarioDao.java
+++ src/br/ifpe/dao/UsuarioDao.java
@@ -1,10 +1,14 @@
 package br.ifpe.dao;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 
+import br.ifpe.basicas.Perfil;
 import br.ifpe.basicas.Usuario;
 import br.ifpe.util.ConnectionFactory;
 
@@ -36,4 +40,41 @@ public class UsuarioDao {
 			throw new RuntimeException(e);
 		}
 	}
-}
+
+	private Usuario montarObjeto(ResultSet rs) throws SQLException {
+
+		Usuario usuario = new Usuario();
+		usuario.setNome(rs.getString("nome"));
+		usuario.setMatricula(rs.getString("matricula"));
+		usuario.setPerfil(Perfil.valueOf(rs.getString("perfil")));
+
+		return usuario;
+	}
+
+	// listar usuario
+		public List<Usuario> listar() {
+
+			try {
+				List<Usuario> listarUsuario = new ArrayList<Usuario>();
+				String sql = "SELECT * FROM usuario ORDER BY nome";
+				PreparedStatement stmt = (PreparedStatement) connection.prepareStatement(sql);
+			
+				ResultSet rs = stmt.executeQuery();
+
+				while (rs.next()) {
+					listarUsuario.add(montarObjeto(rs));
+				}
+
+				rs.close();
+				stmt.close();
+				connection.close();
+
+				return listarUsuario;
+
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+
+}//fim
