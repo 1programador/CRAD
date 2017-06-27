52cef45193aa6ff20e6f09d3d82221a657c2cdee
DouglasGusmao <ruggery402@gmail.com>
2017-06-27 19:59:43 -0300

    Correções no form usuário + form serviço

--- src/br/ifpe/controller/UsuarioController.java
+++ src/br/ifpe/controller/UsuarioController.java
@@ -1,34 +1,40 @@
 package br.ifpe.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.ifpe.basicas.Usuario;
 import br.ifpe.dao.UsuarioDao;
 
-
-
 @Controller
 public class UsuarioController {
 
 	@RequestMapping("/cdu")
-	public String cadastrar() {
+	public String paginaUsuario() {
 		return "usuario/cadastrarUsuario";
 	}
-	
+
+	@RequestMapping("/cds")
+	public String paginaServico() {
+		return "servico/cadastrarServico";
+	}
+
 	@RequestMapping("/login")
 	public String login() {
 		return "index";
 	}
 
 	@RequestMapping("incluirUsuario")
-	public String incluirUsuario(Usuario usuario) {
+	public String incluirUsuario(Usuario usuario, Model model) {
 
 		UsuarioDao dao = new UsuarioDao();
 		dao.inserir(usuario);
-		return "";
+		model.addAttribute("mensagem", "O usuário " + usuario.getNome() + " foi cadastrado com Sucesso !");
+
+		return "forward:cdu";
 	}
-	
+
 	@RequestMapping("/paginaInicial")
 	public String paginaHome() {
 		return "usuario/home";
