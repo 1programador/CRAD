12fc9f37ea6cd519062965c41d4c0874c7a1e6e8
pedro <pdo_hsp@hotmail.com>
2017-06-29 19:41:56 -0300

    sql modificado

--- src/SQL/Script_crad.sql
+++ src/SQL/Script_crad.sql
@@ -5,12 +5,12 @@ use crad;
 
 create table usuario(id int auto_increment primary key,
 nome varchar(50) not null,
-matricula varchar(15) not null,
+matricula varchar(15) not null unique,
 perfil enum('ALUNO','PROFESSOR','COORDENADOR') not null
 );
 
 create table servico(id int auto_increment primary key,
-descricao varchar(150) not null,
+descricao varchar(150) not null unique,
 status tinyint not null default 1,
 anexo tinyint not null default 0,
 complemento tinyint not null default 0
