412bbb6d3c4d8e45ae08532ad8e67c2be867e761
DouglasGusmao <ruggery402@gmail.com>
2017-07-13 19:43:00 -0300

    Novo atributo status

--- src/br/ifpe/basicas/Usuario.java
+++ src/br/ifpe/basicas/Usuario.java
@@ -3,22 +3,23 @@ package br.ifpe.basicas;
 public class Usuario {
 
 	private int id;
+	private boolean status;
 	private String nome;
 	private String matricula;
 	private Perfil perfil;
 
-	public Usuario(String nome, String matricula, Perfil perfil) {
+	public Usuario(int id, boolean status, String nome, String matricula, Perfil perfil) {
 		super();
+		this.id = id;
+		this.status = status;
 		this.nome = nome;
 		this.matricula = matricula;
 		this.perfil = perfil;
 	}
-	
-	public Usuario(){
-		
+
+	public Usuario() {
+
 	}
-	
-	
 
 	public int getId() {
 		return id;
@@ -26,6 +27,14 @@ public class Usuario {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
 	}
 
 	public String getNome() {
@@ -51,6 +60,5 @@ public class Usuario {
 	public void setPerfil(Perfil perfil) {
 		this.perfil = perfil;
 	}
-	
-	
+
 }
