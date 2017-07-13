7d1dbd5585327771d1890c62b59052414318611f
DouglasGusmao <ruggery402@gmail.com>
2017-07-13 20:01:47 -0300

    Alterações no nome da tabela servico=tipo de solicitação

--- src/SQL/Script_crad.sql
+++ src/SQL/Script_crad.sql
@@ -6,12 +6,14 @@ use crad;
 create table usuario(id int auto_increment primary key,
 nome varchar(50) not null,
 matricula varchar(15) not null unique,
+status boolean default true,
 perfil enum('ALUNO','PROFESSOR','COORDENADOR') not null
 );
 
-create table servico(id int auto_increment primary key,
+create table tipo_solicitacao(id int auto_increment primary key,
 descricao varchar(150) not null unique,
 status boolean not null default true,
 anexo boolean not null,
+documentos varchar(50),
 complemento boolean not null
 );
